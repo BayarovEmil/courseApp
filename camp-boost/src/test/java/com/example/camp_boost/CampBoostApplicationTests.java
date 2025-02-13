6006efd5b622b12352fe489a267445ab40a8da79
emil.beyerov <emil.beyerov@digigo.work>
2025-02-13 23:26:29 +0400

    Spring boot project

---
+++ camp-boost/src/test/java/com/example/camp_boost/CampBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camp_boost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
